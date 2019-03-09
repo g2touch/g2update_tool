13ddb1c871c64e33454c27d236943786dd6ec778
Jerry <jerry@g2touch.corp-partner.google.com>
2019-03-09 01:33:46 -0500

    test branch return error code

--- MAIN/exitcode.h
+++ MAIN/exitcode.h
@@ -8,8 +8,8 @@ namespace G2
         EXIT_PASS = 0,                      // execute done. And result pass.
         EXIT_IC_COMMUNICATION_ERROR = 2,    // G2touch-IC communication error. It will need to self-retry.
         EXIT_FLOW_ERROR = 3,                 // check flow/somethings error. User may set some things error.
-        EXIT_FAIL = 98,                      // execute done. But result fail. Such as compare different
-        EXIT_TEST_ERROR = 99                 // check flow/somethings error. User may set some things error.
+        EXIT_FAIL = 53,                      // execute done. But result fail. Such as compare different
+        EXIT_TEST_ERROR = 57                 // check flow/somethings error. User may set some things error.
     };
 }
 
